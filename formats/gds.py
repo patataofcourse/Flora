bc39b61612d43568968e57bb183a5535fd943092
patataofcourse <patataofcourse@gmail.com>
2021-06-14 16:42:13 +0200

    CLI GDS COMMAND

--- formats/gds.py
+++ formats/gds.py
@@ -8,12 +8,19 @@ from version import v
 def cli():
     pass
 
-def unpack(file, outfile):
-    file = open(file, "rb").read()
-    outfile = open(outfile, "w")
+@cli.command(
+                name="unpack",
+                help="converts a GDS script to a readable format",
+                no_args_is_help = True
+            )
+@click.argument("input")
+@click.argument("output")
+def unpack(input, output):
+    input = open(input, "rb").read()
+    output = open(output, "w")
 
-    length = int.from_bytes(file[0:4], "little")
-    cmd_data = file[6:length+4]
+    length = int.from_bytes(input[0:4], "little")
+    cmd_data = input[6:length+4]
     cmds = []
 
     cmd = None
@@ -74,15 +81,22 @@ def unpack(file, outfile):
                 out += repr(param[1])
         out += "\n"
 
-    outfile.write(out.strip())
+    output.write(out.strip())
 
-def pack(file, outfile):
-    file = open(file).read().split("\n")
-    outfile = open(outfile, "wb")
+@cli.command(
+                name="pack",
+                help="creates a GDS file from a readable script",
+                no_args_is_help = True
+            )
+@click.argument("input")
+@click.argument("output")
+def pack(input, output):
+    input = open(input).read().split("\n")
+    output = open(output, "wb")
 
     out = bytearray(b'\x00\x00')
 
-    for line in file:
+    for line in input:
         if line.startswith("#") or line == "":
             continue
         
@@ -117,6 +131,6 @@ def pack(file, outfile):
         out += b'\x00\x00'
     out[-2] = 0xc
     
-    outfile.write(len(out).to_bytes(4, "little"))
-    outfile.write(out)
-    outfile.close()
+    output.write(len(out).to_bytes(4, "little"))
+    output.write(out)
+    output.close()

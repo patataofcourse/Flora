8b585cb7b9a02339c524b7741ed3f7faa86cdbef
patataofcourse <patataofcourse@gmail.com>
2021-06-14 21:47:10 +0200

    Fix some palette stuff

--- formats/bg.py
+++ formats/bg.py
@@ -1,5 +1,6 @@
 import click
 from nlzss.lzss3 import decompress
+from nlzss.compress import compress
 import os
 from PIL import Image
 
@@ -15,36 +16,36 @@ def cli():
 @click.argument("output")
 def extract(input, output):
     input = open(input, "rb").read()
-    output = open(output, "wb")
 
     data = decompress(input[4:])
     p_len = int.from_bytes(data[0:4], "little")
     c = 0
     p_colors = []
     while c < p_len*2:
-        inf = int.from_bytes(data[c+4:c+6], "big")
-        b = inf%2**15//2**10 * 8 
-        g = inf%2**10//32 * 8
-        r = inf%32 *8
+        inf = int.from_bytes(data[c+4:c+6], "little")
+        b = (inf>>10 & 0x1f) * 8
+        g = (inf>>5 & 0x1f) * 8
+        r = (inf & 0x1f) * 8
         p_colors.append([r, g, b])
         c += 2
-    
+
+    data = data[4+2*c:]
+
     while len(p_colors) < 256:
         p_colors.append([255,255,255])
     outcolors = b''
     for c in range(256):
         outcolors += bytes(p_colors[c])
     Image.frombytes("RGB",[16,16],outcolors).save("palette.png")
-    #return #TODO: export the other stuff
 
-    data = data[4+c:]
     num_tiles = int.from_bytes(data[:4], "little")
     tiles = []
     for tile in range(num_tiles):
         curr_tile = []
         for row in range(8):
             curr_row = []
-            for pixel in data[4+tile*0x40+row*8:4+tile*0x40+row*8+8]:
+            pos = 4+tile*0x40+row*8
+            for pixel in data[pos:pos+8]:
                 curr_row.append(pixel)
             curr_tile.append(curr_row)
 

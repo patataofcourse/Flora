eff69be1c6d2a806fcb4b15e70a777c5cc538274
patataofcourse <patataofcourse@gmail.com>
2021-09-06 17:56:44 +0200

    add layton_only arg

--- formats/rom.py
+++ formats/rom.py
@@ -5,7 +5,7 @@ import os
 dir_path = "/".join(os.path.dirname(os.path.realpath(__file__)).split("/")[:-1])
 titles = json.load(open(f"{dir_path}/data/titles.json"))
 
-def load(romfile, long=False):
+def load(romfile, long=False, layton_only=True):
     print("Loading ROM...")
     romfile = rom.NintendoDSRom.fromFile(romfile)
     print("ROM loaded!")
@@ -20,12 +20,13 @@ def load(romfile, long=False):
         title = titles["roms"][id]
     print(f"Game: {title}")
     
-    if id not in titles["supported_roms"]:
-        raise Exception("Currently, this game is not supported by Flora!")
-    elif id not in titles["tested_roms"]:
-        print("\nWarning: this game has not been tested properly with Flora, so errors may arise.")
-        ans = input("Continue? (y/N) ")
-        if ans.lower() != "y":
-            quit()
-    #TODO: check checksum, to see if it's a modified file or not
+    if layton_only:
+        if id not in titles["supported_roms"]:
+            raise Exception("Currently, this game is not supported by Flora!")
+        elif id not in titles["tested_roms"]:
+            print("\nWarning: this game has not been tested properly with Flora, so errors may arise.")
+            ans = input("Continue? (y/N) ")
+            if ans.lower() != "y":
+                quit()
+        #TODO: check checksum, to see if it's a modified file or not
     return romfile, id, title
